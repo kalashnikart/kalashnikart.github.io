6b79230eeada3ab185079ff4308671c11519d51f
Kalashnik Artem <akalashnik@qualium-systems.com>
2022-01-14 17:57:36 +0200

    .

--- app.bundle.js
+++ app.bundle.js
@@ -1,86 +1,6 @@
 "use strict";
 (self["webpackChunkwebxr_image_recognizer"] = self["webpackChunkwebxr_image_recognizer"] || []).push([["app"],{
 
-/***/ "./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js!./src/style.sass":
-/*!*****************************************************************************************************!*\
-  !*** ./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js!./src/style.sass ***!
-  \*****************************************************************************************************/
-/***/ ((module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
-/* harmony export */ });
-/* harmony import */ var _node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/sourceMaps.js */ "./node_modules/css-loader/dist/runtime/sourceMaps.js");
-/* harmony import */ var _node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
-/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
-// Imports
-
-
-var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
-// Module
-___CSS_LOADER_EXPORT___.push([module.id, "body {\n  background: #040d21;\n  margin: 0;\n  color: #627597;\n  display: flex;\n  height: 100vh;\n  width: 100vw;\n}\n\n#root {\n  position: relative;\n  margin: auto;\n  width: 90vw;\n  height: 90vh;\n  overflow: hidden;\n}", "",{"version":3,"sources":["webpack://./src/style.sass"],"names":[],"mappings":"AAKA;EACE,mBAAA;EACA,SAAA;EACA,cANU;EAOV,aAAA;EACA,aAAA;EACA,YAAA;AAJF;;AAaA;EACE,kBAAA;EACA,YAAA;EACA,WAAA;EACA,YAAA;EACA,gBAAA;AAVF","sourcesContent":["$darkblue0: #040d21\n$darkblue: #030915\n$lightblue: #627597\n$lightgreen: #00cfc8\n\nbody\n  background: rgb(4, 13, 33)\n  margin: 0\n  color: $lightblue\n  display: flex\n  height: 100vh\n  width: 100vw\n\n// #player\n//   position: fixed\n//   right: 0\n//   top: 0\n//   transform: scale(.5)\n//   transform-origin: 100% 0\n\n#root\n  position: relative\n  margin: auto\n  width: 90vw\n  height: 90vh\n  overflow: hidden\n"],"sourceRoot":""}]);
-// Exports
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
-
-
-/***/ }),
-
-/***/ "./src/style.sass":
-/*!************************!*\
-  !*** ./src/style.sass ***!
-  \************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
-/* harmony export */ });
-/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
-/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _node_modules_style_loader_dist_runtime_styleDomAPI_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/styleDomAPI.js */ "./node_modules/style-loader/dist/runtime/styleDomAPI.js");
-/* harmony import */ var _node_modules_style_loader_dist_runtime_styleDomAPI_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_styleDomAPI_js__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _node_modules_style_loader_dist_runtime_insertBySelector_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/insertBySelector.js */ "./node_modules/style-loader/dist/runtime/insertBySelector.js");
-/* harmony import */ var _node_modules_style_loader_dist_runtime_insertBySelector_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_insertBySelector_js__WEBPACK_IMPORTED_MODULE_2__);
-/* harmony import */ var _node_modules_style_loader_dist_runtime_setAttributesWithoutAttributes_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/setAttributesWithoutAttributes.js */ "./node_modules/style-loader/dist/runtime/setAttributesWithoutAttributes.js");
-/* harmony import */ var _node_modules_style_loader_dist_runtime_setAttributesWithoutAttributes_js__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_setAttributesWithoutAttributes_js__WEBPACK_IMPORTED_MODULE_3__);
-/* harmony import */ var _node_modules_style_loader_dist_runtime_insertStyleElement_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/insertStyleElement.js */ "./node_modules/style-loader/dist/runtime/insertStyleElement.js");
-/* harmony import */ var _node_modules_style_loader_dist_runtime_insertStyleElement_js__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_insertStyleElement_js__WEBPACK_IMPORTED_MODULE_4__);
-/* harmony import */ var _node_modules_style_loader_dist_runtime_styleTagTransform_js__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/styleTagTransform.js */ "./node_modules/style-loader/dist/runtime/styleTagTransform.js");
-/* harmony import */ var _node_modules_style_loader_dist_runtime_styleTagTransform_js__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_styleTagTransform_js__WEBPACK_IMPORTED_MODULE_5__);
-/* harmony import */ var _node_modules_css_loader_dist_cjs_js_node_modules_sass_loader_dist_cjs_js_style_sass__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! !!../node_modules/css-loader/dist/cjs.js!../node_modules/sass-loader/dist/cjs.js!./style.sass */ "./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js!./src/style.sass");
-
-      
-      
-      
-      
-      
-      
-      
-      
-      
-
-var options = {};
-
-options.styleTagTransform = (_node_modules_style_loader_dist_runtime_styleTagTransform_js__WEBPACK_IMPORTED_MODULE_5___default());
-options.setAttributes = (_node_modules_style_loader_dist_runtime_setAttributesWithoutAttributes_js__WEBPACK_IMPORTED_MODULE_3___default());
-
-      options.insert = _node_modules_style_loader_dist_runtime_insertBySelector_js__WEBPACK_IMPORTED_MODULE_2___default().bind(null, "head");
-    
-options.domAPI = (_node_modules_style_loader_dist_runtime_styleDomAPI_js__WEBPACK_IMPORTED_MODULE_1___default());
-options.insertStyleElement = (_node_modules_style_loader_dist_runtime_insertStyleElement_js__WEBPACK_IMPORTED_MODULE_4___default());
-
-var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_node_modules_sass_loader_dist_cjs_js_style_sass__WEBPACK_IMPORTED_MODULE_6__["default"], options);
-
-
-
-
-       /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_node_modules_sass_loader_dist_cjs_js_style_sass__WEBPACK_IMPORTED_MODULE_6__["default"] && _node_modules_css_loader_dist_cjs_js_node_modules_sass_loader_dist_cjs_js_style_sass__WEBPACK_IMPORTED_MODULE_6__["default"].locals ? _node_modules_css_loader_dist_cjs_js_node_modules_sass_loader_dist_cjs_js_style_sass__WEBPACK_IMPORTED_MODULE_6__["default"].locals : undefined);
-
-
-/***/ }),
-
 /***/ "./src/index.ts":
 /*!**********************!*\
   !*** ./src/index.ts ***!
@@ -88,10 +8,7 @@ var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _style_sass__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.sass */ "./src/style.sass");
-/* harmony import */ var three__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! three */ "./node_modules/three/build/three.module.js");
-/* harmony import */ var mind_ar_dist_mindar_image_three_prod_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! mind-ar/dist/mindar-image-three.prod.js */ "./node_modules/mind-ar/dist/mindar-image-three.prod.js");
-/* harmony import */ var mind_ar_dist_mindar_image_three_prod_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(mind_ar_dist_mindar_image_three_prod_js__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _unity__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./unity */ "./src/unity/index.ts");
 var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -102,45 +19,120 @@ var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _argume
     });
 };
 
-
-
-// import { WebglEngine } from './webglar';
-// import vertexShader from './vert.glsl';
-// import fragmentShader from './frag.glsl';
-// @ts-ignore
-const mindarThree = new window.MINDAR.IMAGE.MindARThree({
-    container: document.querySelector('#root'),
-    // https://hiukim.github.io/mind-ar-js-doc/tools/compile/
-    imageTargetSrc: '/hiro_1.mind'
-});
-const anchor = mindarThree.addAnchor(0);
-const mesh = new three__WEBPACK_IMPORTED_MODULE_2__.Mesh(new three__WEBPACK_IMPORTED_MODULE_2__.BoxGeometry(1, 1, 1, 1), new three__WEBPACK_IMPORTED_MODULE_2__.MeshNormalMaterial());
-anchor.group.add(mesh);
-const start = () => __awaiter(void 0, void 0, void 0, function* () {
-    yield mindarThree.start();
-    mindarThree.renderer.setAnimationLoop(() => {
-        if (anchor.group.visible) {
-            console.log(mindarThree.controller.trackingStates[0]);
+const setControls = (unityInstance) => {
+    document.addEventListener('keydown', event => {
+        if (event.key === 'r') {
+            unityInstance.SendMessage('NewBehaviourScript', 'SetDirRight');
         }
-        mindarThree.renderer.render(mindarThree.scene, mindarThree.camera);
+        if (event.key === 'u') {
+            unityInstance.SendMessage('NewBehaviourScript', 'SetDirUp');
+        }
+    });
+};
+const serializeV3 = (vec3) => {
+    return `${vec3.x},${vec3.y},${vec3.z}`;
+};
+const initARJS = () => __awaiter(void 0, void 0, void 0, function* () {
+    const unityInstance = yield (0,_unity__WEBPACK_IMPORTED_MODULE_0__.initUnity)();
+    // console.log('----', unityInstance);
+    // setControls(unityInstance);
+    const camera = document.querySelector('[camera]');
+    const marker = document.querySelector('a-marker');
+    let check;
+    marker.addEventListener('markerFound', () => {
+        let cameraPosition = camera.object3D.position;
+        let markerPosition = marker.object3D.position;
+        // let distance = cameraPosition.distanceTo(markerPosition);
+        check = setInterval(() => {
+            cameraPosition = camera.object3D.position;
+            markerPosition = marker.object3D.position;
+            unityInstance.SendMessage('NewBehaviourScript', 'SetPosArgs', serializeV3(markerPosition));
+            unityInstance.SendMessage('NewBehaviourScript', 'SetCamPos', serializeV3(cameraPosition));
+            // console.log(serializeV3(markerPosition));
+            // console.log(camera.object3D, marker.object3D);
+        }, 100);
+    });
+    marker.addEventListener('markerLost', () => {
+        clearInterval(check);
     });
 });
-start();
-//   mindarThree.stop();
-//   mindarThree.renderer.setAnimationLoop(null);
-// const tex = new THREE.VideoTexture(player);
-// engine.scene.background = tex;
-// const customMaterial = new THREE.ShaderMaterial({
-//   vertexShader: vertexShader,
-//   fragmentShader: fragmentShader,
-//   side: THREE.DoubleSide,
-//   uniforms: {
-//     time: { value: 0 },
-//     tex: { value: tex }
-//   }
-// });
-// customMaterial.needsUpdate = true;
-// const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2, 2, 100, 100), customMaterial);
+initARJS();
+
+
+/***/ }),
+
+/***/ "./src/unity/index.ts":
+/*!****************************!*\
+  !*** ./src/unity/index.ts ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "initUnity": () => (/* binding */ initUnity)
+/* harmony export */ });
+var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function unityShowBanner(msg, type) {
+    console.log('unity msg:', 'type', type, 'msg', msg);
+}
+var buildUrl = 'Build';
+var loaderUrl = buildUrl + '/pr1.loader.js';
+var config = {
+    dataUrl: buildUrl + '/pr1.data',
+    frameworkUrl: buildUrl + '/pr1.framework.js',
+    codeUrl: buildUrl + '/pr1.wasm',
+    streamingAssetsUrl: 'StreamingAssets',
+    companyName: 'DefaultCompany',
+    productName: 'My project',
+    productVersion: '0.1',
+    showBanner: unityShowBanner
+};
+// By default Unity keeps WebGL canvas render target size matched with
+// the DOM size of the canvas element (scaled by window.devicePixelRatio)
+// Set this to false if you want to decouple this synchronization from
+// happening inside the engine, and you would instead like to size up
+// the canvas DOM size and WebGL render target sizes yourself.
+// config.matchWebGLToCanvasSize = false;
+function loadScript(src) {
+    return new Promise(function (resolve, reject) {
+        var s;
+        s = document.createElement('script');
+        s.src = src;
+        s.onload = resolve;
+        s.onerror = reject;
+        document.body.appendChild(s);
+    });
+}
+const initUnity = () => __awaiter(void 0, void 0, void 0, function* () {
+    var container = document.querySelector('#unity-container');
+    var canvas = document.querySelector('#unity-canvas');
+    var loadingBar = document.querySelector('#unity-loading-bar');
+    var progressBarFull = document.querySelector('#unity-progress-bar-full');
+    var fullscreenButton = document.querySelector('#unity-fullscreen-button');
+    // var warningBanner = document.querySelector('#unity-warning') as HTMLElement;
+    let unityInstanceX;
+    yield loadScript(loaderUrl);
+    //@ts-ignore
+    yield createUnityInstance(canvas, config)
+        .then((unityInstance) => {
+        unityInstanceX = unityInstance;
+        //init call
+        loadingBar.style.display = 'none';
+        fullscreenButton.onclick = () => {
+            unityInstance.SetFullscreen(1);
+        };
+    })
+        .catch((message) => alert(message));
+    return unityInstanceX;
+});
 
 
 /***/ })
@@ -148,8 +140,7 @@ start();
 },
 /******/ __webpack_require__ => { // webpackRuntimeModules
 /******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ __webpack_require__.O(0, ["vendors-node_modules_css-loader_dist_runtime_api_js-node_modules_css-loader_dist_runtime_sour-a19e5f"], () => (__webpack_exec__("./src/index.ts")));
-/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.ts"));
 /******/ }
 ]);
 //# sourceMappingURL=app.bundle.js.map
